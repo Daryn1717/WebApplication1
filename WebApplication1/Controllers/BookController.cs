1bf78a7708ca757394488b6fceb82f741e9dbb78
ЗатовД <@DESKTOP-936A9GP>
2019-08-21 21:05:58 +0600

    just simple changes

--- WebApplication1/Controllers/BookController.cs
+++ WebApplication1/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,47 @@ namespace WebApplication1.Controllers
                 books = db.Books.ToList();
             }
             return View(books);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(Books book)
+        {
+            using (Model1 db = new Model1())
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Edit(int id,string title)
+        {
+            Books book;
+            using (Model1 db = new Model1())
+            {
+                book = db.Books.FirstOrDefault(b => b.Id == id && b.Title == title); 
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Books book)
+        {
+            using (Model1 db = new Model1())
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
